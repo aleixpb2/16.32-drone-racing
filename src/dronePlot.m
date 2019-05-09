207e6e2899d880951e89a0b3ba3da9c4ae71bddc
Aleix Paris <aleixpb2@gmail.com>
2019-05-09 17:11:58 -0400

    plots finished. Ready to wrap the project up!

--- src/dronePlot.m
+++ src/dronePlot.m
@@ -62,36 +62,53 @@ end
 f = figure();
 subplot(2,3,1)
 hold on
-plot(time_tot, x_tot, 'LineWidth', 1.5)
+for i = 1:n_gates
+    plot(solution.phase(i).time, solution.phase(i).state(:,1), 'LineWidth', 1.5)
+end
+plot(0,0,'*', 'LineWidth', 2)
 plot(time_gates, xgates_tot, 'o')
 title('x vs t'), xlabel('t'), ylabel('x')
 subplot(2,3,2)
 hold on
-plot(time_tot, y_tot, 'LineWidth', 1.5)
+for i = 1:n_gates
+    plot(solution.phase(i).time, solution.phase(i).state(:,2), 'LineWidth', 1.5)
+end
+plot(0,0,'*', 'LineWidth', 2)
 plot(time_gates, ygates_tot, 'o')
 title('y vs t'), xlabel('t'), ylabel('y')
 subplot(2,3,3)
 hold on
-plot(time_tot, z_tot, 'LineWidth', 1.5)
+for i = 1:n_gates
+    plot(solution.phase(i).time, solution.phase(i).state(:,3), 'LineWidth', 1.5)
+end
+plot(0,0,'*', 'LineWidth', 2)
+plot(time_gates, zgates_tot, 'o')
 title('z vs t'), xlabel('t'), ylabel('z')
 subplot(2,3,4)
 hold on
-plot(time_tot, vel_x_tot, 'LineWidth', 1.5)
+for i = 1:n_gates
+    plot(solution.phase(i).time, solution.phase(i).state(:,4), 'LineWidth', 1.5)
+end
 title('v_x vs t'), xlabel('t'), ylabel('v_x')
 subplot(2,3,5)
 hold on
-plot(time_tot, vel_y_tot, 'LineWidth', 1.5)
+for i = 1:n_gates
+    plot(solution.phase(i).time, solution.phase(i).state(:,5), 'LineWidth', 1.5)
+end
 title('v_y vs t'), xlabel('t'), ylabel('v_y')
 subplot(2,3,6)
 hold on
-plot(time_tot, vel_z_tot, 'LineWidth', 1.5)
+for i = 1:n_gates
+    plot(solution.phase(i).time, solution.phase(i).state(:,6), 'LineWidth', 1.5)
+end
 title('v_z vs t'), xlabel('t'), ylabel('v_z')
 
 % 3D plot
 f = figure();
 plot3(x_tot, y_tot, z_tot, 'LineWidth', 1.5)
 hold on
-scatter3(xgates_tot, ygates_tot, zgates_tot);
+scatter3(0,0,0, '*','LineWidth', 7); % start
+scatter3(xgates_tot, ygates_tot, zgates_tot, 'LineWidth', 2);
 tmpAspect=daspect();
 daspect(tmpAspect([1 1 1]))
 xlabel('x'), ylabel('y'), zlabel('z')
@@ -119,13 +136,22 @@ end
 title('Yaw vs t'), xlabel('t'), ylabel('Yaw (deg)')
 
 subplot(2,3,4)
-plot(time_tot, p_tot.*180./pi, 'LineWidth', 1.5)
+hold on
+for i = 1:n_gates
+    plot(solution.phase(i).time, solution.phase(i).state(:,10).*180./pi, 'LineWidth', 1.5)
+end
 title('Roll rate vs t'), xlabel('t'), ylabel('Roll rate (deg/s)')
 subplot(2,3,5)
-plot(time_tot, q_tot.*180./pi, 'LineWidth', 1.5)
+hold on
+for i = 1:n_gates
+    plot(solution.phase(i).time, solution.phase(i).state(:,11).*180./pi, 'LineWidth', 1.5)
+end
 title('Pitch rate vs t'), xlabel('t'), ylabel('Pitch rate (deg/s)')
 subplot(2,3,6)
-plot(time_tot, r_tot.*180./pi, 'LineWidth', 1.5)
+hold on
+for i = 1:n_gates
+    plot(solution.phase(i).time, solution.phase(i).state(:,12).*180./pi, 'LineWidth', 1.5)
+end
 title('Yaw rate vs t'), xlabel('t'), ylabel('Yaw rate (deg/s)')
 
 %% Inputs
